1c768c1a02948400b114f6f6d2ef2df5dff63ef7
yankuan <348972503@qq.com>
2017-08-06 17:03:52 +0800

    second

--- WebRoot/index2.jsp
+++ WebRoot/index2.jsp
@@ -3,12 +3,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
   <head>
-    <title>My JSP 'index.jsp' starting page</title>
+    <title>barChart</title>
   </head>
   
   <body>
     
-    <img alt="" src="pieChart">
+    <img alt="" src="barChart">
     
   </body>
 </html>
--- src/yk/BarChartAction.java
+++ src/yk/BarChartAction.java
@@ -1,65 +1,103 @@
 package yk;
 
-import java.awt.Font;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
+import java.awt.Color;
+import java.io.FileOutputStream;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
-import org.jfree.chart.plot.PiePlot;
-import org.jfree.chart.plot.PiePlot3D;
-import org.jfree.chart.title.TextTitle;
-import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.util.Rotation;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer3D;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.general.DatasetUtilities;
+import org.jfree.ui.TextAnchor;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
-
+/**
+ * 
+ * 保存图片至项目目录下
+ *
+ */
 public class BarChartAction extends ActionSupport{
 
-	private static final long serialVersionUID = 1L;
-	
 	private JFreeChart chart;
 	public JFreeChart getChart() {
 		return chart;
 	}
-
+	
+	@SuppressWarnings("deprecation")
 	@Override
 	public String execute() throws Exception {
 		
-		DefaultPieDataset data = new DefaultPieDataset();
+		double [][]data = new double[][]{{1320,1000,800,700},{720,600,900,200},{830,1100,850,790},{1400,1050,200,450}};
+		String []rowKeys = {"苹果","香蕉","橘子","梨子"}; 
+		String []columnKeys = {"深圳","厦门","北京","上海"};
 		
-		data.setValue("苹果",100); 
-		data.setValue("梨子",200); 
-		data.setValue("葡萄",300); 
-		data.setValue("香蕉",400); 
-		data.setValue("荔枝",500); 
+		CategoryDataset dataset = DatasetUtilities.createCategoryDataset(rowKeys, columnKeys, data);
+		
+        chart = ChartFactory.createBarChart3D(
+        		"水果产量图", // 图表标题
+                "水果", // 目录轴的显示标签
+                "产量", // 数值轴的显示标签
+                 dataset, // 数据集
+                 PlotOrientation.VERTICAL, // 图表方向：水平、垂直
+                 true,  // 是否显示图例(对于简单的柱状图必须是 false)
+                 false, // 是否创建工具提示 (tooltip) 
+                 false  // 是否生成 URL 链接
+                 ); 
         
-        chart = ChartFactory.createPieChart3D(
-        		"水果产量图",  		// 图表标题
-		        data, 
-		        true, 			// 是否显示图例
-		        false, 			// 是否创建工具提示 (tooltip) 
-                false  			// 是否生成 URL 链接
-		        ); 
-        
-        // 副标题
-		chart.addSubtitle(new TextTitle("2013年度"));
-		//显示百分比
-		PiePlot pieplot = (PiePlot)chart.getPlot();
-        pieplot.setLabelFont(new Font("宋体", 0, 12));
-        pieplot.setNoDataMessage("无数据");
-        pieplot.setCircular(true);
-        pieplot.setLabelGap(0.02D);
-        pieplot.setLabelGenerator(new StandardPieSectionLabelGenerator("{0} {2}",NumberFormat.getNumberInstance(),new DecimalFormat("0.00%")));
-        
-        PiePlot3D pieplot3d = (PiePlot3D)chart.getPlot(); 
-		//设置开始角度  
-		pieplot3d.setStartAngle(120D);  
-		//设置方向为”顺时针方向“  
-		pieplot3d.setDirection(Rotation.CLOCKWISE);  
-		//设置透明度，0.5F为半透明，1为不透明，0为全透明  
-		pieplot3d.setForegroundAlpha(0.7F); 
+        CategoryPlot plot = chart.getCategoryPlot();
+        // 设置网格背景颜色
+ 		plot.setBackgroundPaint(Color.white);
+ 		// 设置网格竖线颜色
+ 		plot.setDomainGridlinePaint(Color.pink);
+ 		// 设置网格横线颜色
+ 		plot.setRangeGridlinePaint(Color.pink);
+ 		
+ 		// 显示每个柱的数值，并修改该数值的字体属性
+ 		BarRenderer3D renderer=new BarRenderer3D();
+ 		renderer.setBaseItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+ 		renderer.setBaseItemLabelsVisible(true);
+ 		
+ 		renderer.setBasePositiveItemLabelPosition(new ItemLabelPosition(ItemLabelAnchor.OUTSIDE12, TextAnchor.BASELINE_LEFT));
+ 		renderer.setItemLabelAnchorOffset(10D);  
+ 		
+ 		// 设置平行柱的之间距离
+ 		renderer.setItemMargin(0.4);
+ 		plot.setRenderer(renderer);
+ 		
+ 		
+ 		/**
+ 		 * 保存图片至tomcat服务器中该项目下
+ 		 */
+ 		HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+ 		String realPath2 = request.getSession().getServletContext().getRealPath("/");
+// 		C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\JFreeChart_Struts2\
+ 		
+ 		 FileOutputStream fos_jpg = null; 
+         long currentTimeMillis = System.currentTimeMillis();
+         try { 
+         	//将图片保存至Tomcat服务器WebRoot下的img目录中
+             fos_jpg = new FileOutputStream(realPath2+currentTimeMillis+".jpg");
+             ChartUtilities.writeChartAsJPEG(fos_jpg,1,chart,500,400,null); 
+         } catch (Exception e) {
+         	System.out.println("error");
+ 		} finally { 
+             try { 
+                 fos_jpg.close(); 
+             } catch (Exception e) {
+             	System.out.println("error2");
+             } 
+         }
+ 		
 		
 		return SUCCESS;
 	}
